af91d99c7f939b07fbdca203b646199b8c89b0f8
mbecker12 <marvinbecker@mail.de>
2021-02-17 15:32:13 +0100

    changed imports back

--- tests/test_step.py
+++ tests/test_step.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from surface_rl_decoder.surface_code import SurfaceCode
+from src.surface_rl_decoder.surface_code import SurfaceCode
 
 def test_step_function():
     pass
