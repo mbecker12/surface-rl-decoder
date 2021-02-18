393a07df8b7464c37846794cb6d887d8d1cffc27
mbecker12 <marvinbecker@mail.de>
2021-02-18 22:50:13 +0100

    test syndrome calculation for multiple qb errors

--- tests/test_syndrome_output.py
+++ tests/test_syndrome_output.py
@@ -1,142 +1,192 @@
+import os
 import numpy as np
 from src.surface_rl_decoder.surface_code import SurfaceCode
 
 
 def test_syndrome_output(v=False):
-    scode = SurfaceCode()
+    sc = SurfaceCode()
 
-    scode.qubits[0, 1, 1] = 1
+    sc.qubits[0, 1, 1] = 1
 
     expected_syndrome = np.zeros(
-        (scode.system_size + 1, scode.system_size + 1), dtype=np.uint8
+        (sc.system_size + 1, sc.system_size + 1), dtype=np.uint8
     )
     expected_syndrome[1, 1] = 1
     expected_syndrome[2, 2] = 1
 
-    syndrome = scode.create_syndrome_output(scode.qubits[0])
+    syndrome = sc.create_syndrome_output(sc.qubits[0])
 
     if v:
-        print(f"{scode.qubits=}")
+        print(f"{sc.qubits=}")
         print(f"{syndrome=}")
         print(f"{expected_syndrome=}")
     assert np.all(syndrome == expected_syndrome)
 
-    scode.qubits[0, 3, 2] = 1
+    sc.qubits[0, 3, 2] = 1
     expected_syndrome[3, 3] += 1
     expected_syndrome[4, 2] += 1
 
-    syndrome = scode.create_syndrome_output(scode.qubits[0])
+    syndrome = sc.create_syndrome_output(sc.qubits[0])
 
     if v:
-        print(f"{scode.qubits=}")
+        print(f"{sc.qubits=}")
         print(f"{syndrome=}")
         print(f"{expected_syndrome=}")
     assert np.all(syndrome == expected_syndrome % 2)
 
     # edge cases
-    scode.qubits[0, 3, 4] = 1
+    sc.qubits[0, 3, 4] = 1
     expected_syndrome[3, 5] += 1
     expected_syndrome[4, 4] += 1
 
-    syndrome = scode.create_syndrome_output(scode.qubits[0])
+    syndrome = sc.create_syndrome_output(sc.qubits[0])
 
     if v:
-        print(f"{scode.qubits=}")
+        print(f"{sc.qubits=}")
         print(f"{syndrome=}")
         print(f"{expected_syndrome=}")
     assert np.all(syndrome == expected_syndrome % 2)
 
-    scode.qubits[0, 2, 2] = 3  # z error
+    sc.qubits[0, 2, 2] = 3  # z error
     expected_syndrome[2, 3] += 1
     expected_syndrome[3, 2] += 1
 
-    syndrome = scode.create_syndrome_output(scode.qubits[0])
+    syndrome = sc.create_syndrome_output(sc.qubits[0])
 
     if v:
-        print(f"{scode.qubits=}")
+        print(f"{sc.qubits=}")
         print(f"{syndrome=}")
         print(f"{expected_syndrome=}")
     assert np.all(syndrome == expected_syndrome % 2)
 
-    scode.qubits[0, 1, 3] = 2
+    sc.qubits[0, 1, 3] = 2
     expected_syndrome[1, 3] += 1
     expected_syndrome[1, 4] += 1
     expected_syndrome[2, 3] += 1
     expected_syndrome[2, 4] += 1
 
-    syndrome = scode.create_syndrome_output(scode.qubits[0])
+    syndrome = sc.create_syndrome_output(sc.qubits[0])
 
     if v:
-        print(f"{scode.qubits=}")
+        print(f"{sc.qubits=}")
         print(f"{syndrome=}")
         print(f"{expected_syndrome=}")
     assert np.all(syndrome == expected_syndrome % 2)
 
 
 def test_syndrome_output_edge(v=True):
-    scode = SurfaceCode()
+    sc = SurfaceCode()
     expected_syndrome = np.zeros(
-        (scode.system_size + 1, scode.system_size + 1), dtype=np.uint8
+        (sc.system_size + 1, sc.system_size + 1), dtype=np.uint8
     )
 
-    scode.qubits[0, 0, 0] = 2
+    sc.qubits[0, 0, 0] = 2
 
     expected_syndrome[0, 1] += 1
     expected_syndrome[1, 1] += 1
 
-    syndrome = scode.create_syndrome_output(scode.qubits[0])
+    syndrome = sc.create_syndrome_output(sc.qubits[0])
 
     if v:
-        print(f"{scode.qubits=}")
+        print(f"{sc.qubits=}")
         print(f"{syndrome=}")
         print(f"{expected_syndrome=}")
     assert np.all(syndrome == expected_syndrome % 2)
 
-    scode.qubits[0, 2, 0] = 2
+    sc.qubits[0, 2, 0] = 2
     expected_syndrome[2, 0] += 1
     expected_syndrome[2, 1] += 1
     expected_syndrome[3, 1] += 1
 
-    syndrome = scode.create_syndrome_output(scode.qubits[0])
+    syndrome = sc.create_syndrome_output(sc.qubits[0])
 
     if v:
-        print(f"{scode.qubits=}")
+        print(f"{sc.qubits=}")
         print(f"{syndrome=}")
         print(f"{expected_syndrome=}")
     assert np.all(syndrome == expected_syndrome % 2)
 
 
-# TODO
-# def test_all_syndromes(v=True):
-#     def reset():
-#         scode = SurfaceCode()
-#         expected_syndrome = np.zeros(
-#             (scode.system_size + 1, scode.system_size + 1), dtype=np.uint8
-#         )
-#         return scode, expected_syndrome
+def test_multiple_syndromes_x(sc):
+    expected_syndrome = np.zeros(
+        (sc.system_size + 1, sc.system_size + 1), dtype=np.uint8
+    )
 
-#     scode = SurfaceCode()
-#     d = scode.system_size
+    original_size = os.environ.get("CONFIG_ENV_SIZE")
+    os.environ["CONFIG_ENV_SIZE"] = "5"
+    sc = SurfaceCode()
+    d = sc.system_size
 
-#     assert d == 5
+    # need to restrict system size here to be able to check boundaries
+    assert d == 5
 
-#     # X errors
-#     # 0,0
-#     scode, expected_syndrome = reset()
-#     scode.qubits[0, 0] = 1
-#     expected_syndrome[]
+    # X errors
+    # 0,0
+    sc.qubits[0, 0, 0] = 1
+    expected_syndrome[1, 1] += 1
+
+    # 0,2
+    sc.qubits[0, 0, 2] = 1
+    expected_syndrome[1, 3] += 1
+
+    # 1,4
+    sc.qubits[0, 1, 4] = 1
+    expected_syndrome[1, 5] += 1
+    expected_syndrome[2, 4] += 1
+
+    # 2,2
+    sc.qubits[0, 2, 2] = 1
+    expected_syndrome[2, 2] += 1
+    expected_syndrome[3, 3] += 1
+
+    # 4,0
+    sc.qubits[0, 4, 0] = 1
+    expected_syndrome[4, 0] += 1
+
+    syndrome = sc.create_syndrome_output(sc.qubits[0])
+    assert np.all(syndrome == expected_syndrome % 2)
+
+    os.environ["CONFIG_ENV_SIZE"] = original_size
 
 
-#     # X errors
-#     for i in range(d):
-#         for j in range(d):
-#             scode = SurfaceCode()
-#             expected_syndrome = np.zeros(
-#                 (scode.system_size + 1, scode.system_size + 1), dtype=np.uint8
-#             )
+def test_multiple_syndromes_z(sc):
+    expected_syndrome = np.zeros(
+        (sc.system_size + 1, sc.system_size + 1), dtype=np.uint8
+    )
 
-#             scode.qubits[i, j] = 1
-#             expected_syndrome[]
+    original_size = os.environ.get("CONFIG_ENV_SIZE")
+    os.environ["CONFIG_ENV_SIZE"] = "5"
+    sc = SurfaceCode()
+    d = sc.system_size
+
+    # need to restrict system size here to be able to check boundaries
+    assert d == 5
+
+    # Z errors
+    # 4,1
+    sc.qubits[0, 4, 1] = 3
+    expected_syndrome[4, 1] += 1
+    expected_syndrome[5, 2] += 1
+
+    # 2,2
+    sc.qubits[0, 2, 2] = 3
+    expected_syndrome[3, 2] += 1
+    expected_syndrome[2, 3] += 1
+
+    # 1,4
+    sc.qubits[0, 1, 3] = 3
+    expected_syndrome[1, 4] += 1
+    expected_syndrome[2, 3] += 1
+
+    # 4,4
+    sc.qubits[0, 4, 4] = 3
+    expected_syndrome[5, 4] += 1
+
+    syndrome = sc.create_syndrome_output(sc.qubits[0])
+
+    assert np.all(syndrome == expected_syndrome % 2)
+
+    os.environ["CONFIG_ENV_SIZE"] = original_size
 
 
 if __name__ == "__main__":
