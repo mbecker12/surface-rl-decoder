3c74c0806ae834db86f8be56d310326bd29a361f
mbecker12 <marvinbecker@mail.de>
2021-02-18 15:22:39 +0100

    Implemented visualization

---
+++ tests/test_visualization.py
@@ -0,0 +1,13 @@
+from surface_rl_decoder.surface_code import SurfaceCode
+
+
+def test_qubit_grid():
+    sc = SurfaceCode()
+
+    sc.render(block=False)
+
+    print("Hallo")
+
+
+if __name__ == "__main__":
+    test_qubit_grid()
