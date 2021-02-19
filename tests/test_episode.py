a9347277efcad109fa6a7c1df0168b1f67b6738d
mbecker12 <marvinbecker@mail.de>
2021-02-19 17:10:10 +0100

    black [skip ci]

--- tests/test_episode.py
+++ tests/test_episode.py
@@ -1,6 +1,11 @@
 from copy import deepcopy
 import numpy as np
+import os, sys
+
+sys.path.append(os.getcwd())
+from src.surface_rl_decoder.surface_code import SurfaceCode
 from src.surface_rl_decoder.surface_code_util import TERMINAL_ACTION
+from tests.data_episode_test import _actual_errors, _qubits, _state, _syndrome_errors
 
 
 def test_episode(sc):
@@ -23,7 +28,9 @@ def test_episode(sc):
     assert sc.qubits.sum() != 0
 
     sc.actual_errors = deepcopy(sc.qubits)
+    assert sc.actual_errors.sum() != 0
 
+    # assert that errors have been applied
     for i in range(sc.qubits.shape[0] - 1):
         assert sc.qubits[i + 1].sum() >= sc.qubits[i].sum(), np.vstack(
             (sc.qubits[i], sc.qubits[i + 1])
@@ -33,11 +40,12 @@ def test_episode(sc):
     for action in actions:
         previous_qb_sum = sc.qubits.sum()
         sc.step(action)
-
         assert previous_qb_sum >= sc.qubits.sum()
 
+    # send terminal action to stop the episode
     sc.step(action=(9, 9, TERMINAL_ACTION))
 
+    # assure that the action history corresponds to the created actions
     for i, action in enumerate(actions):
         assert np.all(sc.actions[i] == action)
 
@@ -52,5 +60,108 @@ def test_episode(sc):
     assert sc.qubits[h_max:].sum() == 0, sc.qubits
 
     sc.step(action=(9, 9, TERMINAL_ACTION))
+    assert sc.qubits[h_max:].sum() == 0, sc.qubits
+
+
+def test_episode_w_measurement_errors(sc, block=False):
+    sc.p_error = 0.0
+    sc.p_msmt = 0.0
+    sc.reset()
+
+    assert sc.qubits.sum() == 0
+    assert sc.state.sum() == 0
+    assert sc.next_state.sum() == 0
+
+    actions = [(0, 0, 0), (1, 2, 3), (4, 3, 2), (0, 1, 1), (3, 0, 2)]
+
+    h_max = sc.stack_depth - len(actions) + 1
+
+    # prepare qubit grid
+    for i, action in enumerate(actions):
+        row, col, operator = action[:]
+        sc.qubits[i:, row, col] = operator
+
+    assert sc.qubits.sum() != 0
+
+    sc.actual_errors = deepcopy(sc.qubits)
+    assert sc.actual_errors.sum() != 0
+
+    sc.render(block=block)
+
+    # introduce measurement errors
+    sc.syndrome_errors = deepcopy(sc.state)
+    sc.syndrome_errors[1, 3, 5] = 1
+    sc.syndrome_errors[2, 3, 4] = 1
+    sc.syndrome_errors[4, 1, 2] = 1
+    sc.syndrome_errors[:2, 4, 1] = 1
+    sc.syndrome_errors[6:, 4, 1] = 1
+
+    # apply actions to resolve qubit errors
+    for action in actions:
+        previous_qb_sum = sc.qubits.sum()
+        sc.step(action)
+        assert previous_qb_sum >= sc.qubits.sum()
+
+    assert sc.state[1, 1, 1] == 1
+    assert sc.state[1, 4, 0] == 1
+    assert sc.state[1, 3, 1] == 1
+    assert sc.state[1, 4, 1] == 0
+    assert sc.state[1, 4, 3] == 1
+    assert sc.state[1, 4, 4] == 1
+    assert sc.state[1, 3, 5] == 1
+    assert sc.syndrome_errors[1, 3, 5] == 1
+    assert sc.actual_errors[1, 3, 4] == 0
+    assert sc.actual_errors[1, 2, 4] == 0
+
+    assert sc.state[2, 4, 1] == 1
+    assert sc.state[2, 1, 1] == 1
+    assert sc.state[2, 3, 4] == 1
+
+    assert sc.state[4, 1, 2] == 1
+    assert sc.state[6, 4, 1] == 1
 
     assert sc.qubits[h_max:].sum() == 0, sc.qubits
+
+    sc.step(action=(9, 9, TERMINAL_ACTION))
+    assert sc.qubits[h_max:].sum() == 0, sc.qubits
+
+    sc.render(block=block)
+
+
+def test_proper_episode():
+    """
+    Test a proper episode where no human inference occurs
+    which could corrupt and disturb copy actions etc.
+    """
+
+    original_depth = os.environ.get("CONFIG_ENV_STACK_DEPTH", "4")
+    os.environ["CONFIG_ENV_STACK_DEPTH"] = "4"
+    original_size = os.environ.get("CONFIG_ENV_SIZE", "5")
+    os.environ["CONFIG_ENV_SIZE"] = "5"
+    original_error_channel = os.environ.get("CONFIG_ENV_ERROR_CHANNEL", "x")
+    os.environ["CONFIG_ENV_ERROR_CHANNEL"] = "x"
+
+    sc = SurfaceCode()
+    np.random.seed(42)
+    sc.p_error = 0.1
+    sc.p_msmt = 0.1
+    sc.reset(error_channel="dp")
+
+    assert sc.stack_depth == 4
+    assert sc.system_size == 5
+
+    assert np.all(sc.qubits == _qubits), sc.qubits
+    assert np.all(sc.syndrome_errors == _syndrome_errors), sc.syndrome_errors
+    assert np.all(sc.actual_errors == _actual_errors), sc.actual_errors
+    assert np.all(sc.state == _state), sc.state
+
+    os.environ["CONFIG_ENV_STACK_DEPTH"] = original_depth
+    os.environ["CONFIG_ENV_SIZE"] = original_size
+    os.environ["CONFIG_ENV_ERROR_CHANNEL"] = original_error_channel
+
+
+if __name__ == "__main__":
+    from surface_rl_decoder.surface_code import SurfaceCode
+
+    sc = SurfaceCode()
+    test_episode_w_measurement_errors(sc, block=True)
