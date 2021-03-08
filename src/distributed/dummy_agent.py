d01a611a437b03188f038b36fb2f285dd258a5fe
mbecker12 <marvinbecker@mail.de>
2021-03-08 19:19:03 +0100

    refine dummy agent

--- src/distributed/dummy_agent.py
+++ src/distributed/dummy_agent.py
@@ -1,20 +1,28 @@
-import torch
 from torch import nn
+import torch
 import torch.nn.functional as F
 
+
 class DummyModel(nn.Module):
-    def __init__(self, syndrome_size, stack_depth):
+    def __init__(self, syndrome_size, stack_depth, num_actions_per_qubit=3):
         super().__init__()
-        self.lin1 = nn.Linear(stack_depth * syndrome_size * syndrome_size, 512, bias=True)
-        self.lin2 = nn.Linear(512, 512)
+        self.syndrome_size = syndrome_size
+        self.stack_depth = stack_depth
         n_qubits = syndrome_size - 1
-        n_actions_total = 3 * n_qubits * n_qubits + 1
+
+        self.lin1 = nn.Linear(
+            stack_depth * syndrome_size * syndrome_size, 512, bias=True
+        )
+        self.lin2 = nn.Linear(512, 512)
+        n_actions_total = num_actions_per_qubit * n_qubits * n_qubits + 1
         self.output = nn.Linear(512, n_actions_total)
 
     # pylint: disable=invalid-name
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
+        # flatten the syndrome stack as a dummy operation to make the shapes fit
+        x = x.view((-1, self.stack_depth * self.syndrome_size * self.syndrome_size))
         x = F.relu(self.lin1(x))
         x = F.relu(self.lin2(x))
         x = self.output(x)
 
-        return F.sigmoid(x)
+        return x
