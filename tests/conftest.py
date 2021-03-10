a829a9c4333714ddcb795f0890a4d5ccc3259aaf
mbecker12 <marvinbecker@mail.de>
2021-03-10 15:47:07 +0100

    added tests for util functions in distributed

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,10 @@
     - https://docs.pytest.org/en/stable/writing_plugins.html
 """
 import os
+import json
 import pytest
 import numpy as np
+from src.distributed.model_util import choose_model, extend_model_config
 from src.surface_rl_decoder.surface_code import SurfaceCode
 from src.distributed.environment_set import EnvironmentSet
 
@@ -60,6 +62,37 @@ def seed_surface_code():
 
     return seed_sc
 
+
 @pytest.fixture
 def env_set():
     return EnvironmentSet(SurfaceCode(), 5)
+
+
+@pytest.fixture
+def init_testing_model():
+    def testing_model(args, state_size, stack_depth):
+        model_name = args["model_name"]
+        model_config = args["model_config"]
+        model_config = extend_model_config(model_config, state_size, stack_depth)
+        model = choose_model(model_name, model_config)
+        return model
+
+    return testing_model
+
+
+MODEL_CONFIG_LOCATION = "src/distributed/model_spec"
+
+
+@pytest.fixture
+def load_model_config():
+    def get_config_json(model_config_file, model_name):
+        model_config_file_path = os.path.join(MODEL_CONFIG_LOCATION, model_config_file)
+        with open(model_config_file_path) as json_file:
+            model_config = json.load(json_file)
+
+        # select the specification of the right model from the json
+        model_config = model_config[model_name]
+
+        return model_config
+
+    return get_config_json
