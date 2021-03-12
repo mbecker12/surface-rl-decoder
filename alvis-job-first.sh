4859d7e0b343f89fce5160c41d86352c927fb4d2
mbecker12 <marvinbecker@mail.de>
2021-03-12 21:56:24 +0100

    Update alvis test script; Now with GPU support!

--- alvis-job-first.sh
+++ alvis-job-first.sh
@@ -2,27 +2,34 @@
 #SBATCH -J qec-test
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 0:05:00
+#SBATCH -t 0:00:40
 #SBATCH -A SNIC2020-33-2 -p alvis
 #SBATCH --gpus-per-node=V100:1
 #SBATCH --output=./logs-sbatch/logs-%j.out
 
-echo "Starting job on cluster"
+echo "###### Starting job on cluster"
+echo "Learner device: ${DISTRIBUTED_CONFIG_LEARNER_DEVICE}"
+echo "System size: ${CONFIG_ENV_SIZE}"
 
-SINGULARITY_IMAGE_NAME=qec-mp.sif
+IMAGE_WORKDIR=surface-rl-decoder
+CLUSTER_WORKDIR=surface-rl-decoder
+LOG_PATH=${HOME}/${CLUSTER_WORKDIR}/tmp_runs
+
+SINGULARITY_IMAGE_NAME=${CLUSTER_WORKDIR}/qec-mp.sif
 DOCKER_IMAGE_NAME=docker://xero32/qec-mp:first
-
-WORKDIR=surface-rl-decoder
-LOG_PATH=${HOME}/${WORKDIR}/tmp_runs
 
 mkdir -p ${LOG_PATH}
 
 job_stats.py ${SLURM_JOB_ID}
 
-singularity run \
-    -B ${LOG_PATH}:/${WORKDIR}/runs:rw \
-    ${DOCKER_IMAGE_NAME} \
+singularity run --nv \
+    -B ${LOG_PATH}:/${IMAGE_WORKDIR}/runs:rw \
+    --env-file ${CLUSTER_WORKDIR}/conf.env \
+    ${SINGULARITY_IMAGE_NAME} \
     /bin/bash -c \
-    "cd /${WORKDIR}; \
+    "cd /${IMAGE_WORKDIR}; \
     python --version; \
-    python /${WORKDIR}/src/distributed/start_distributed_mp.py"
+    nvidia-smi;
+    python -c 'import torch; print(torch.cuda.is_available())'; \
+    python -c 'import torch.cuda as tc; id = tc.current_device(); print(tc.get_device_name(id))'; \
+    python /${IMAGE_WORKDIR}/src/distributed/start_distributed_mp.py"
