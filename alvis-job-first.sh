d3d75cb37932621660bf24e21076b6e310e255b8
mbecker12 <marvinbecker@mail.de>
2021-03-16 15:19:40 +0100

    updated job script; support local testing and cluster deployment

--- alvis-job-first.sh
+++ alvis-job-first.sh
@@ -4,7 +4,7 @@
 #SBATCH -n 1
 #SBATCH -t 0:00:20
 #SBATCH -A SNIC2020-33-2 -p alvis
-#SBATCH --gpus-per-node=V100:1
+#SBATCH --gpus-per-node=T4:1
 #SBATCH --output=./logs-sbatch/logs-%j.out
 
 echo "###### Starting job on cluster"
@@ -13,23 +13,36 @@ echo "System size: ${CONFIG_ENV_SIZE}"
 
 IMAGE_WORKDIR=surface-rl-decoder
 CLUSTER_WORKDIR=surface-rl-decoder
-LOG_PATH_SPECIFICATION=tmp_runs
+TENSORBOARD_PATH_SPECIFICATION=tmp_runs
+NETWORK_SAVE_PATH_CLUSTER=tmp_networks
+LOG_PATH_SPECIFICATION_CLUSTER=tmp_runs
+NETWORK_SAVE_PATH_IMAGE=networks
+LOG_PATH_SPECIFICATION_IMAGE=runs
 
-mkdir -p ${LOG_PATH}
 
 if [ -z "${SLURM_JOB_ID}" ]
     then
     echo "No Slurm Job ID found. Revert to local testing mode."
-    CLUSTER_WORKDIR="."
+    CLUSTER_WORKDIR=$(pwd)
+    LOG_PATH=${CLUSTER_WORKDIR}/${LOG_PATH_SPECIFICATION_CLUSTER}
+    NETWORK_PATH=${CLUSTER_WORKDIR}/${NETWORK_SAVE_PATH_CLUSTER}
 else
     echo "Slurm Job ID is ${SLURM_JOB_ID}." 
     job_stats.py ${SLURM_JOB_ID}
+    LOG_PATH=${HOME}/${CLUSTER_WORKDIR}/${LOG_PATH_SPECIFICATION_CLUSTER}
+    NETWORK_PATH=${HOME}/${CLUSTER_WORKDIR}/${NETWORK_SAVE_PATH_CLUSTER}
 fi
-
-LOG_PATH=${HOME}/${CLUSTER_WORKDIR}/${LOG_PATH_SPECIFICATION}
 
 SINGULARITY_IMAGE_NAME=${CLUSTER_WORKDIR}/qec-mp.sif
 DOCKER_IMAGE_NAME=docker://xero32/qec-mp:first
+
+echo ""
+echo "Network saving path on host: ${NETWORK_PATH}"
+echo "Tensorboard path on host: ${LOG_PATH}"
+echo ""
+
+mkdir -p ${LOG_PATH}
+mkdir -p ${NETWORK_PATH}
 
 if [ -z "$1" ]
     then
@@ -39,7 +52,8 @@ if [ -z "$1" ]
     echo ""
 
     singularity run --nv \
-        -B ${LOG_PATH}:/${IMAGE_WORKDIR}/runs:rw \
+        -B ${LOG_PATH}:/${IMAGE_WORKDIR}/${LOG_PATH_SPECIFICATION_IMAGE}:rw \
+        -B ${NETWORK_PATH}:/${IMAGE_WORKDIR}/${NETWORK_SAVE_PATH_IMAGE}:rw \
         ${SINGULARITY_IMAGE_NAME} \
         /bin/bash -c \
         "cd /${IMAGE_WORKDIR}; \
@@ -55,6 +69,7 @@ else
 
     singularity run --nv \
         -B ${LOG_PATH}:/${IMAGE_WORKDIR}/runs:rw \
+        -B ${NETWORK_PATH}:/${IMAGE_WORKDIR}/networks:rw \
         --env-file $1 \
         ${SINGULARITY_IMAGE_NAME} \
         /bin/bash -c \
