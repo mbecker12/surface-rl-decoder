b5b20d309130b5d500d8572d0ce2b0e255d5464d
mbecker12 <marvinbecker@mail.de>
2021-03-17 15:56:38 +0100

    implemented prioritized replay memory & sum tree

---
+++ src/distributed/sum_tree.py
@@ -0,0 +1,122 @@
+"""
+Implementation of a sum tree
+to be used in prioritized replay memory.
+
+Based on https://github.com/Lindeby/toric-RL-decoder/blob/master/src/SumTree.py
+"""
+import math
+
+
+class SumTree(object):
+    """
+    Initialize a sum tree object to store
+    data points in a tree structure.
+    Used for prioritized replay memory
+    """
+
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.tree_level = math.ceil(math.log(max_size + 1, 2)) + 1
+        self.tree_size = 2 ** self.tree_level - 1
+        self.tree = [0 for i in range(self.tree_size)]
+        self.data = [None for i in range(self.max_size)]
+        self.size = 0
+        self.cursor = 0
+
+    def add(self, contents, value):
+        """
+        Add an item to the tree.
+
+        Parameters
+        ==========
+        contents: The data object to store at a location in the tree
+        value: Value corresponding to the contents to add to the tree,
+            used as priorization value
+        """
+        index = self.cursor
+        self.cursor = (self.cursor + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+        self.data[index] = contents
+        self.val_update(index, value)
+
+    def get_val(self, index):
+        """
+        Return the value at a spcific index
+
+        Parameters
+        ==========
+        index: requested index
+
+        Returns
+        =======
+        value: Value at requested index
+        """
+        tree_index = 2 ** (self.tree_level - 1) - 1 + index
+        return self.tree[tree_index]
+
+    def val_update(self, index, value):
+        """
+        Update the value at a specific index
+
+        Parameters
+        ==========
+        index: The tree index for the new value
+        value: New value
+        """
+        tree_index = 2 ** (self.tree_level - 1) - 1 + index
+        diff = value - self.tree[tree_index]
+        self.reconstruct(tree_index, diff)
+
+    def reconstruct(self, tindex, diff):
+        """
+        # TODO No idea
+        """
+        # TODO docu
+        self.tree[tindex] += diff
+        if not tindex == 0:
+            tindex = int((tindex - 1) / 2)
+            self.reconstruct(tindex, diff)
+
+    def find(self, value, norm=True):
+        """"""
+        # TODO docu
+        if norm:
+            value *= self.tree[0]
+        return self._find(value, 0)
+
+    def _find(self, value, index):
+        """"""
+        # TODO docu
+        if 2 ** (self.tree_level - 1) - 1 <= index:
+            data_index = index - (2 ** (self.tree_level - 1) - 1)
+            assert 0 <= data_index < len(self.data), \
+                f"{data_index=}, {len(self.data)=}, {self.tree_level=}, {index=}\n\n \
+                    {self.print_tree()}"
+            return (
+                self.data[data_index],
+                self.tree[index],
+                data_index,
+            )
+
+        left = self.tree[2 * index + 1]
+
+        if value <= left:
+            return self._find(value, 2 * index + 1)
+        else:
+            return self._find(value - left, 2 * (index + 1))
+
+    def print_tree(self):
+        """
+        Print the current realization of the tree.
+        """
+        for k in range(1, self.tree_level + 1):
+            for j in range(2 ** (k - 1) - 1, 2 ** k - 1):
+                print(self.tree[j], end=" ")
+            print()
+
+    def filled_size(self):
+        """
+        Return the number of elements currently in the tree.
+        """
+        return self.size
