451c5c2c0499d9b2c78d550384b85aa21256e123
falckk <45255513+falckk@users.noreply.github.com>
2021-04-24 23:45:22 +0200

    mostly done, had to revamp and add quite a few things after realising that running multiple did matter more than I thought, most of the old code could still be used however

--- src/HER/ReplayBuffer.py
+++ src/HER/ReplayBuffer.py
@@ -5,10 +5,11 @@ import random
 import torch
 import numpy as np
 
+
 #may want to readjust to be able to take multiple environments
 class ReplayBuffer:
 
-    def __init__(self, buffer_size, batch_size, device, seed, gamma, n_step = 1):
+    def __init__(self, buffer_size, batch_size, device, gamma, n_step = 1):
         """Initialize a ReplayBuffer object.
         Params
         ======
@@ -16,26 +17,26 @@ class ReplayBuffer:
             batch_size: (int) size of each training batch
             seed: (int) random seed
             n_step: (int) steps for the single step buffer before it creates an experience of n_steps
+                used if one desires a multi-step experience
         """
 
         self.device = device
         self.memory = deque(maxlen = buffer_size)
         self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names = ["state", "action", "reward", "next_state", "done"])
-        random.seed(seed)
+        self.experience = namedtuple("Experience", field_names = ["state", "action", "reward", "next_state", "done", "goal"])
         self.gamma = gamma
         self.n_step = n_step
         self.n_step_buffer = deque(maxlen = self.n_step)
 
 
-    def add(self, state, action, reward, next_state, done):
+    def add(self, state, action, reward, next_state, done, goal):
         """Add a new experience to memory"""
-        #print("before:", state, action, reward, next_state, done)
-        self.n_step_buffer.append((state, action, reward, next_state, done))
+        #print("before:", state, action, reward, next_state, done, goal)
+        self.n_step_buffer.append((state, action, reward, next_state, done, goal))
         if len(self.n_step_buffer) == self.n_step:
             state, action, reward, next_state, done = self.calc_multistep_return()
             #print("after:",state, action, reward, next_state, done)
-            e = self.experience(state, action, reward, next_state, done)
+            e = self.experience(state, action, reward, next_state, done, goal)
             self.memory.append(e)
 
     
@@ -47,7 +48,39 @@ class ReplayBuffer:
         #return first of the experience state, action, expected return from the action, final state, and done at the end
         return self.n_step_buffer[0][0], self.n_step_buffer[0][1], Return, self.n_step_buffer[-1][3], self.n_step_buffer[-1][4] 
 
-    def sample(self):
+    #adjusts the replay buffer for hindsight learning for the last inputted episode
+    def new_goals(self, buffer_idx, buffer_size, local_buffer_transitions, local_buffer_qvalues, n):
+        new_goals = []
+        indexes = []
+        #create new goals
+        for idx, exp in enumerate(local_buffer_transitions[buffer_idx,:buffer_size]):
+            for _ in range(n):
+                random_index = random.randint(idx, buffer_size)
+                indexes.append(random_index)
+                transition = local_buffer_transitions[buffer_idx, random_index]
+                new_goal = transition[0]
+                new_goals.append(new_goal)
+        for step in range(buffer_size):
+            transition = local_buffer_transitions[buffer_idx, step]
+            s = transition[0]
+            a = np.argmax(local_buffer_qvalues[buffer_idx, step])
+            ns = transition[3]
+            for new_goal in new_goals:
+                
+                r = self.reward_function(transition[0], new_goal)
+                
+                if (next_state == new_goal).all():
+                    d = 1
+                else:
+                    d = 0
+                self.add(s, a, r, ns, d, new_goal)
+        
+
+    def reward_function(self, state, goal):
+        return 0 if (state == goal).all() else -1
+
+
+    def sample(self): #check that the sample stacking end up correct
         """Randomly sample a batch of experiences from memory. """
         experiences = random.sample(self.memory, k = self.batch_size)
 
@@ -56,8 +89,16 @@ class ReplayBuffer:
         rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
         next_states = torch.from_numpy(np.stack([e.next_state for e in experiences if e is not None])).float().to(self.device)
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
+        goals = torch.from_numpy(np.stack([e.goal for e in experiences if e is not None])).float().to(self.device)
 
-        return (states, actions, rewards, next_states, dones)
+        #print(states[1])
+        #print(actions[1])
+        #print(rewards[1])
+        #print(next_states[1])
+        #print(dones[1])
+        #print(goals[1])
+        return (states, actions, rewards, next_states, dones, goals)
+
 
     def __len__(self):
         """Return current size of memory"""
--- src/HER/hindsight3.py
+++ src/HER/hindsight3.py
@@ -25,7 +25,8 @@ from surface_rl_decoder.surface_code import SurfaceCode
 import ReplayBuffer
 import DQN_Agent
 
-Transition = namedtuple("Transition", ["state", "action", "reward", "next_state", "terminal"])
+
+Transition = namedtuple("Transition", ["state", "action", "reward", "next_state", "terminal", "goal"])
 
 def hindsight3(self, config):
     """
@@ -37,10 +38,10 @@ def hindsight3(self, config):
     Parameters
     ==========
     n_episodes: (int) maximum number of training episodes
+    epoch_steps: (int) number of steps in an epoch before it goes over to the training section
     size_action_history: (int) maximum size of the action history of the environment,
             trying to execute more actions than this in one environment causes the environment
             to terminate and start again with a new syndrome.
-    size_local_memory_buffer: local memory buffer size
     num_actions_per_qubit: (int) number of possible operators in a qubit
     verbosity: (int) verbosity level
     batch_size: (int) size of batches
@@ -62,6 +63,7 @@ def hindsight3(self, config):
     id: (int) id for process for identification
     update_every: (int) update frequency
     n_step: (int) n_step for how far one should consider the rewards
+    n: (int) how many extra goals to have
     load_model: (int) 1 or 0, toggle on whether to download a pretrained model or not
     old_model_path: (str) if load_model is 1, then load from this path
     
@@ -72,7 +74,6 @@ def hindsight3(self, config):
     """
 
 
-    writer = SummaryWriter("runs/" + "BF_HER_4_")
 
     hindsight_id = config["id"]
     n_episodes = int(config.get("n_episodes"))
@@ -100,7 +101,8 @@ def hindsight3(self, config):
     device = str(config.get("device"))
     hindsight_id = int(config.get("id"))
     update_every = int(config.get("update_every", 4))
-    n_step = int(config.get(n_step, 1))
+    n_step = int(config.get("n_step", 1))
+    n = int(config.get("n",4))
 
 
     model_name = str(config.get("model_name"))
@@ -123,42 +125,32 @@ def hindsight3(self, config):
     #create a collection of independent environments
     environments = EnvironmentSet(env, num_environments)
 
-    #Set the true goal
-    true_goal = np.zeros(stack_depth, code_size, code_size)
-
     transition_type = np.dtype(
         [
             ("state", (np.uint8, (stack_depth, state_size, state_size))),
-            ("action", (np.uint8, 3)),
+            ("action", (np.uint8, int)),
             ("reward", float),
             ("next_state", (np.uint8, (stack_depth, state_size, state_size))),
             ("terminal", bool),
+            ("goal", (np.uint8, (stack_depth, state_size, state_size)))
         ]
     )
 
-    #initialize all the environments
+    #Set the true goal
+    true_goal = np.zeros(stack_depth, state_size, state_size)
 
+    #initialize all the environments
     states = environments.reset_all()
     steps_per_episode = np.zeros(num_environments)
 
-    #initialize local memory buffers
+    #initialize local memory buffer
     size_local_memory_buffer = config.get("size_local_memory_buffer") + 1
-    local_buffer_transitions = np.empty(
-        (num_environments, size_local_memory_buffer), dtype=transition_type
-    )
-    local_buffer_actions = np.empty(
-        (num_environments, size_local_memory_buffer, 3), dtype=np.uint8
-    )
-    local_buffer_qvalues = np.empty(
-        (num_environments, size_local_memory_buffer),
-        dtype=(float, num_actions_per_qubit * code_size * code_size + 1),
-    )
-    local_buffer_rewards = np.empty(
-        (num_environments, size_local_memory_buffer), dtype=float
-    )
-    buffer_idx = 0
+    local_buffer_transitions = np.empty((num_environments, size_local_memory_buffer), dtype = transition_type)
+    local_buffer_qvalues = np.empty((num_environments, size_local_memory_buffer), dtype = (float, num_actions_per_qubit * code_size * code_size + 1))
 
-    replay_buffer = ReplayBuffer()
+    buffer_idx = np.zeros(num_environments)
+    
+    replay_buffer = ReplayBuffer(buffer_size, batch_size, device, gamma, n_step)
 
 
     #initalize agent
@@ -167,11 +159,6 @@ def hindsight3(self, config):
     )
     qnetwork_target = choose_model(model_name, model_config)
     qnetwork_local = choose_model(model_name, model_config)
-
-    # load communication queues
-    #actor_io_queue = args["actor_io_queue"]
-    #learner_actor_queue = args["learner_actor_queue"]
-
 
     if load_model_flag == 1:
         qnetwork_target, _, _ = load_model(qnetwork_target, old_model_path)
@@ -186,13 +173,16 @@ def hindsight3(self, config):
     heartbeat_interval = 60 #seconds
 
     logger.info(f"Hindsight {hindsight_id} starting to loop in {device}")
-    #sent_data_chunks = 0
+
 
     #initialize tensorboard for monitoring/logging
     tensorboard = SummaryWriter(os.path.join(summary_path, str(code_size), summary_date, "hindsight"))
     tensorboard_step = 0
     steps_to_benchmark = 0
     benchmark_frequency = 1000
+
+    epoch_steps = int(config.get("epoch_steps", 100000)) 
+    steps_in_this_epoch = 0
 
     while True:
         steps_per_episode += 1
@@ -211,7 +201,7 @@ def hindsight3(self, config):
 
         actions, q_values = select_actions(_states, qnetwork_target, code_size, epsilon = annealed_epsilon
         
-        if benchmarking and steps_to_benchmark % benchmark_frequency == 0:
+        if (benchmarking and steps_to_benchmark % benchmark_frequency == 0):
             select_action_stop = time()
             logger.info(f"time for select action: {select_action_stop-select_action_start}")
 
@@ -241,21 +231,86 @@ def hindsight3(self, config):
             delta_t,
             walltime = current_time_tb)
         
+        
+        #send data to the local buffer and if episode complete also to the replay buffer
         transitions = np.asarray(
             [
-            Transition(
-                states[i], actions[i], reward[i], next_states[i], terminals[i]
-            ) for i in range(num_environments)
-        ], dtype = transition_type)
-
-        local_buffer_transitions[:buffer_idx] = transitions
-        local_buffer_actions[:, buffer_idx] = actions
-        local_buffer_qvalues[:, buffer_idx] = q_values
-        local_buffer_rewards[:, buffer_idx] = rewards
+                Transition(
+                    states[i], actions[i], rewards[i], next_states[i], terminals[i], goals[i]
+                )
+                for i in range(num_environments)
+            ],
+            dtype = transition_type,
+        )
+        for i in range(num_environments):
+            local_buffer_transitions[i, buffer_idx[i]] = transitions[i]
+            local_buffer_qvalues[i, buffer_idx[i]] = q_values[i]
         buffer_idx += 1
 
 
-        #prepare to send local transitions to replay memory
-        if buffer_idx >= size_local_memory_buffer:
-            #get new weights for the policy model here
-            if (learner_qsize :=  learner)
+
+
+        #for environ in range(num_environments):
+        #    replay_buffer.add(states[environ,:,:,:], np.argmax(q_values[environ,:]), rewards[environ], next_states[environ,:,:,:], terminals[environ])
+            
+        if verbosity >= 4:
+            tensorboard.add_scalar("hindsight/transitions",
+            num_environments * steps_in_this_epoch,
+            delta_t,
+            walltime = current_time_tb)
+            tensorboard_step += 1
+
+
+        #if anywhere has a finished episode or there is too much data then push it to the memory
+        if np.any(buffer_idx) >= size_local_memory_buffer or np.any(terminals):
+            indices = argwhere(np.logical_or(terminals, buffer_idx >= size_local_memory_buffer)).flatten
+            for index in indices:
+                for step in range(buffer_idx[index]+1):
+                    replay_buffer.add(local_buffer_transitions[index,step][0], #state
+                    np.argmax(local_buffer_qvalues[index,step]), #action in terms of theneural network
+                    local_buffer_transitions[index, step][2], # reward
+                    local_buffer_transitions[index,step][3], #next_state
+                    local_buffer[index, step][4],  #terminal
+                    local_buffer[index, step][5]) #goal 
+                replay_buffer.new_goals(index, buffer_idx[index], local_buffer_transitions, local_buffer_qvalues, n)
+                buffer_idx[index] = 0
+                
+
+        too_many_steps = steps_per_episode > size_action_history
+
+        if np.any(terminals) or np.any(too_many_steps):
+            #find terminal envs
+            indices = np.argwhere(np.logical_or(terminals, too_many_steps)).flatten
+
+            reset_states = environments.reset_terminal_environments(indices = indices)
+            next_states[indices] = reset_states[indices]
+            steps_per_episode[indices] = 0
+
+            #write to the score window
+
+        states = next_states
+        environments.states = deepcopy(states)
+
+        if time() - heart > heartbeat_interval:
+            heart = time()
+            logger.debug("It's alive")
+
+
+        steps_in_this_epoch += 1
+        if steps_in_this_epoch == epoch_steps:
+            break
+
+
+    
+    for training_step in range(epoch_steps):
+        samples = replay_buffer.sample()
+
+
+
+
+
+
+
+
+
+
