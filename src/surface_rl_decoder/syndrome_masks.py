02eef14ec643e852bc8bfb49f923cf669b9aaeb4
mbecker12 <marvinbecker@mail.de>
2021-05-08 13:07:59 +0200

    make code size and stack depth configurable on call to SurfaceCode

--- src/surface_rl_decoder/syndrome_masks.py
+++ src/surface_rl_decoder/syndrome_masks.py
@@ -66,6 +66,16 @@ vertex_mask_torch = torch.tensor(vertex_mask, device=device)
 vertex_mask_torch_float = torch.tensor(vertex_mask, device=device, dtype=torch.float32)
 vertex_mask_torch_int = torch.tensor(vertex_mask, device=device, dtype=torch.int64)
 
+def get_vertex_mask(d):
+    vertex_mask = np.zeros((d + 1, d + 1), dtype=np.uint8)
+    for i in range(1, d):
+        if i % 2 == 1:
+            for j in range(1, d + 1, 2):
+                vertex_mask[i, j] = 1
+        else:
+            for j in range(0, d + 1, 2):
+                vertex_mask[i, j] = 1
+    return vertex_mask
 """
 Need to cover the following indices in a d=7 surface code to denote plaquettes
 [
@@ -114,6 +124,21 @@ for j in range(1, d - 1, 2):
     plaquette_mask[0, j] = 1
 for j in range(2, d + 1, 2):
     plaquette_mask[d, j] = 1
+
+def get_plaquette_mask(d):
+    plaquette_mask = np.zeros((d + 1, d + 1), dtype=np.uint8)
+    for i in range(1, d):
+        if i % 2 == 1:
+            for j in range(2, d + 1, 2):
+                plaquette_mask[i, j] = 1
+        else:
+            for j in range(1, d, 2):
+                plaquette_mask[i, j] = 1
+    for j in range(1, d - 1, 2):
+        plaquette_mask[0, j] = 1
+    for j in range(2, d + 1, 2):
+        plaquette_mask[d, j] = 1
+    return plaquette_mask
 
 plaquette_mask_torch = torch.tensor(plaquette_mask, device=device)
 plaquette_mask_torch_float = torch.tensor(
