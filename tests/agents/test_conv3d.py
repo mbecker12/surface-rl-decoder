7f7f94fb9ad7f02e0113b2e71555782e23d842aa
mbecker12 <marvinbecker@mail.de>
2021-05-12 14:52:16 +0200

    add tests for 3dconv agent in ppo

--- tests/agents/test_conv3d.py
+++ tests/agents/test_conv3d.py
@@ -112,3 +112,36 @@ def test_conv3d_agent_w_opposite_split_toggle():
     output = model(state)
 
     assert output.shape == (batch_size, 3 * code_size * code_size + 1)
+
+def test_conv3d_agent_ppo():
+    model_name = "conv3d"
+    for model_conf_file_path in (
+        "src/config/model_spec/conv_agents_slim.json",
+        "src/config/model_spec/conv_agents.json",
+        "src/config/model_spec/conv_agents_large.json"
+    ):
+        with open(model_conf_file_path) as model_conf_file:
+            model_config = json.load(model_conf_file)[model_name]
+        model_config = add_model_size(model_config, model_conf_file_path)
+        model_config["rl_type"] = "ppo"
+        syndrome_size = 6
+        code_size = syndrome_size - 1
+        stack_depth = 8
+        model_config = extend_model_config(model_config, syndrome_size, stack_depth)
+        model = choose_model(model_name, model_config)
+        assert model.rl_type == "ppo"
+
+        batch_size = 16
+        state = np.random.randint(
+            0, 2, size=(batch_size, stack_depth, syndrome_size, syndrome_size)
+        )
+        state = torch.tensor(state, dtype=torch.float32)
+        output, value = model(state)
+
+        assert value.shape == (batch_size, 1)
+
+        for param_tensor in model.state_dict():
+            print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+        
+        # assert 0
+        assert output.shape == (batch_size, 3 * code_size * code_size + 1)
