ba5c33f2962e7ab7040c75e74f7e6f7e78823a9a
mbecker12 <marvinbecker@mail.de>
2021-05-20 00:01:19 +0200

    changes to stack depth analysis routine

--- analysis/analyze_stack_depth.py
+++ analysis/analyze_stack_depth.py
@@ -1,5 +1,6 @@
 # theoretically, we have all the runs for d=3,5,7
 # Still some uncertainties in error rate (p_{err} or p_{err}^{one_layer})
+from typing import Dict
 from analysis_util import analyze_succesful_episodes
 import os
 import sys
@@ -35,6 +36,8 @@ job_ids = [
     70287
 ]
 
+omit_job_ids = []
+
 stack_depths = [3, 5, 7, 9, 11]
 
 job_id_mapping = {jid: stack_depths[i] for i, jid in enumerate(job_ids)}
@@ -64,22 +67,20 @@ if do_copy:
                 continue
 
 df_all_stats = pd.DataFrame(columns=[
-    "jobid", "code_size", "stack_depth", "p_err_train", "p_err", 
-    "success_rate", "logical_err_rate", "remaining_syndrome_rate", "ground_state_rate",
-    "solved_w_syndrome_rate", "n_episodes"
+    "jobid", "code_size", "stack_depth", "p_err_train", "p_err"
     ])
 
 all_results_counter = 0
-n_episodes = 512
+n_episodes = 128
 model_name = "conv3d"
 eval_device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 if torch.cuda.is_available():
     LOCAL_NETWORK_PATH = "/surface-rl-decoder/networks"
 
-run_evaluation = False
-load_eval_results = True
+run_evaluation = True
+load_eval_results = False
 produce_plots = False
-csv_file_path = "analysis/depth2_analysis_results_small_p_err.csv"
+csv_file_path = "analysis/depth2_analysis_results_p_err.csv"
 
 max_num_of_steps = 32
 if run_evaluation:
@@ -109,6 +110,7 @@ if run_evaluation:
             print("Load Model")
             try:
                 # model = choose_old_model(model_name, model_config)
+                # TODO: may have to choose old model
                 model = choose_model(model_name, model_config)
                 model, _, _ = load_model(model, old_model_path, model_device=eval_device)
             except Exception as err:
@@ -118,19 +120,12 @@ if run_evaluation:
                 
                 continue
             p_error_list = np.arange(start=0.0001, stop=0.0120, step=0.0005)
-            for p_err in p_error_list:
+            for p_idx, p_err in enumerate(p_error_list):
+                sys.stdout.write(f"\r{p_idx + 1:02d} / {len(p_error_list):02d}")
                 p_msmt = p_err
                 # batch_evaluation(..., p_error, p_msmt)
-                
-                (
-                    total_n_episodes,
-                    counter_successful_episodes,
-                    counter_logical_errors,
-                    counter_syndrome_left,
-                    counter_ground_state,
-                    counter_solved_w_syndrome_left,
-                    n_steps_arr
-                ) = analyze_succesful_episodes(
+
+                result_dict: Dict = analyze_succesful_episodes(
                     model,
                     "",
                     device=eval_device,
@@ -144,25 +139,18 @@ if run_evaluation:
                     stack_depth=stack_depth
                 )
 
+                result_dict["jobid"]= jid
+                result_dict["code_size"]= code_size
+                result_dict["stack_depth"]= stack_depth
+                result_dict["p_err_train"]= p_err_train
+                result_dict["p_err"]= p_err
+                result_dict["avg_steps"] = result_dict["n_steps_arr"].mean()
+                result_dict.pop("n_steps_arr")
                 
                 # save relevant eval stats to dataframe
-                df_all_stats = df_all_stats.append(
-                    {
-                        "jobid": jid,
-                        "code_size": code_size,
-                        "stack_depth": stack_depth,
-                        "p_err_train": p_err_train,
-                        "p_err": p_err,
-                        "success_rate": counter_successful_episodes / total_n_episodes,
-                        "logical_err_rate": counter_logical_errors / total_n_episodes,
-                        "remaining_syndrome_rate": counter_syndrome_left / total_n_episodes,
-                        "ground_state_rate": counter_ground_state / total_n_episodes,
-                        "solved_w_syndrome_rate": counter_solved_w_syndrome_left / total_n_episodes,
-                        "avg_steps": n_steps_arr.mean(),
-                        "n_episodes": n_episodes
-                    },
-                    ignore_index=True
-                )
+                df_all_stats = df_all_stats.append(result_dict, ignore_index=True)
+            print()
+            print()
     print("Saving dataframe...")
     if os.path.exists(csv_file_path):
         df_all_stats.to_csv(csv_file_path, mode='a', header=False)
@@ -172,7 +160,9 @@ if run_evaluation:
 if load_eval_results:
     # migt need to fix indices and so on
     # df_all_stats = pd.read_csv("analysis/analysis_results2.csv")
+    print("Load Data File")
     df_all_stats = pd.read_csv(csv_file_path, index_col=0)
+    print(f"{df_all_stats=}")
 
 if not produce_plots:
     print("Not producing result plot. Exiting...")
@@ -185,58 +175,262 @@ dfs = [df_all_stats.loc[(df_all_stats["jobid"] == jid) | (df_all_stats["jobid"] 
 new_dfs = []
 
 eval_key_list = [
-            "success_rate",
-            "logical_err_rate",
-            "remaining_syndrome_rate",
+            "total_n_episodes",
+            "n_ground_states",
+            "n_valid_episodes",
+            "n_valid_ground_states",
+            "n_valid_non_trivial_loops",
+            "n_ep_w_syndromes",
+            "n_ep_w_loops",
+            "n_too_long",
+            "n_too_long_w_loops",
+            "n_too_long_w_syndromes",
             "avg_steps"
         ]
 
-agg_key_list = ["weighted_" + key for key in eval_key_list]
+agg_key_list = [key for key in eval_key_list]
 
 for df in dfs:
-    df = df.sort_values(by="success_rate", ascending=True)
+    print(f"{df=}")
+    if int(df["jobid"].iloc[0]) == 69308:
+        continue
+    if int(df["jobid"].iloc[0]) == 71571:
+        continue
+
+    df = df.sort_values(by="n_ground_states", ascending=True)
+
+    #TODO: aggregate / sum values first
     df["expected_n_err"] = df["p_err"] * df["code_size"] * df["code_size"] * df["stack_depth"]
     df["p_err_one_layer"] = df["p_err"] * df["stack_depth"]
+    df["avg_steps"] = df["avg_steps"] * df["total_n_episodes"]
 
-    for j, agg_key in enumerate(agg_key_list):
-        df[agg_key] = df[eval_key_list[j]] * df["n_episodes"]
-    
     aggregation_dict = {agg_key: ["sum"] for agg_key in agg_key_list}
-    aggregation_dict["n_episodes"] = ["sum"]
     aggregation_dict["code_size"] = ["last"]
-    aggregation_dict["stack_depth"] = ["mean"]
-    aggregation_dict["p_err"] = ["mean"]
-    aggregation_dict["expected_n_err"] = ["mean"]
-    aggregation_dict["p_err_one_layer"] = ["mean"]
+    aggregation_dict["stack_depth"] = ["last"]
+    aggregation_dict["p_err"] = ["last"]
+    aggregation_dict["expected_n_err"] = ["last"]
+    aggregation_dict["p_err_one_layer"] = ["last"]
+    
     groups = df.groupby(by="p_err")
     agg_groups = groups.agg(aggregation_dict)
-    print(f"{type(agg_groups)=}")
+
     new_df = pd.DataFrame()
-    for agg_key in agg_key_list:
-        agg_groups[agg_key] /= agg_groups["n_episodes"]
+
+    agg_groups["weighted_avg_steps"] = agg_groups["avg_steps"] / agg_groups["total_n_episodes"]
 
     agg_groups.columns = agg_groups.columns.droplevel(1)
     
     print(agg_groups)
 
-    agg_groups["fail_rate"] = 1 - agg_groups["weighted_success_rate"]
-    agg_groups["scaled_fail_rate"] = agg_groups["fail_rate"] / agg_groups["stack_depth"]
-    
+    agg_groups["valid_success_rate"] = agg_groups["n_valid_ground_states"] / agg_groups["n_valid_episodes"]
+    agg_groups["overall_success_rate"] = (agg_groups["n_ground_states"] + agg_groups["n_ep_w_syndromes"]) / agg_groups["total_n_episodes"]
+
+    agg_groups["valid_fail_rate"] = 1.0 - agg_groups["valid_success_rate"]
+    agg_groups["overall_fail_rate"] = 1.0 - agg_groups["overall_success_rate"]
+
+    agg_groups["valid_fail_rate_per_cycle"] = agg_groups["valid_fail_rate"] / agg_groups["stack_depth"]
+    agg_groups["overall_fail_rate_per_cycle"] = agg_groups["overall_fail_rate"] / agg_groups["stack_depth"]
+
+    agg_groups["validity_rate"] = agg_groups["n_valid_episodes"] / agg_groups["total_n_episodes"]
+
+    agg_groups["valid_avg_lifetime"] = 1.0 / agg_groups["valid_fail_rate_per_cycle"]
+    agg_groups["overall_avg_lifetime"] = 1.0 / agg_groups["overall_fail_rate_per_cycle"]
+
+    # agg_groups["fail_rate"] = 1 - agg_groups["weighted_success_rate"]
+    # agg_groups["scaled_fail_rate"] = agg_groups["fail_rate"] / agg_groups["stack_depth"]
+
     new_dfs.append(agg_groups)
+
 df_all = pd.concat(new_dfs)
+print(df_all)
 # need to filter best performing model for each d as well
 
-# fig, ax = plt.subplots(len(eval_key_list), 1, sharex=True)
-# for ax_idx, key in enumerate(eval_key_list):
-#     for i, jid in enumerate(job_ids):
-#         code_size = new_dfs[i]["code_size"].iloc[0]
-#         p_err = new_dfs[i]["p_err"].iloc[0]
-#         # print(new_dfs[i])
-#         ax[ax_idx].scatter(x=new_dfs[i]["stack_depth"], y=new_dfs[i][key], label=f"d={code_size}, jid={jid}, p_err={p_err}")
-#         ax[ax_idx].set(title=key, xlabel="stack_depth")
+error_rates = df_all["p_err"]
+key_success_rate = "weighted_success_rate"
+title_succes_rate = "Success Rate"
+key_scaled_fail_rate = "overall_fail_rate_per_cycle"
+title_scaled_fail_rate = "Overall Fail Rate Per Cycle"
 
-# plt.legend()
-# plt.show()
+key_valid_fail_rate = "valid_fail_rate_per_cycle"
+title_valid_fail_rate = "Valid Fail Rate Per Cycle"
+
+key_valid_avg_life = "valid_avg_lifetime"
+title_valid_avg_life = "Valid Average Lifetime"
+
+key_overall_avg_life = "overall_avg_lifetime"
+title_overall_avg_life = "Overall Average Lifetime"
+
+for o_jid in omit_job_ids:
+    job_ids.remove(o_jid)
+
+################## Plot Overall Fail Rate per Cycle ##################
+fig, ax = plt.subplots(1, 1, sharex=True)
+
+for i, jid in enumerate(job_ids):
+    if jid in omit_job_ids:
+        continue
+
+    code_size = new_dfs[i]["code_size"].iloc[0]
+    stack_depth = new_dfs[i]["stack_depth"].iloc[0]
+    # print(new_dfs[i])
+    ax.scatter(
+        x=new_dfs[i]["p_err"],
+        y=new_dfs[i][key_scaled_fail_rate],
+        label=f"d={code_size}, h={stack_depth}")
+ax.plot(
+    np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    'k',
+    label="One Qubit"
+)
+ax.set(title=title_scaled_fail_rate)
+ax.set(xlabel=r"$p_\mathrm{err}$", ylabel=title_scaled_fail_rate)
+
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("plots/depth_overall_fail_rate_p_err.pdf")
+plt.show()
+
+################## Plot Valid Fail Rate per Cycle ##################
+fig, ax = plt.subplots(1, 1, sharex=True)
+
+for i, jid in enumerate(job_ids):
+    code_size = new_dfs[i]["code_size"].iloc[0]
+    stack_depth = new_dfs[i]["stack_depth"].iloc[0]
+    # print(new_dfs[i])
+    ax.scatter(
+        x=new_dfs[i]["p_err"],
+        y=new_dfs[i][key_valid_fail_rate],
+        label=f"d={code_size}, h={stack_depth}")
+ax.plot(
+    np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    'k',
+    label="One Qubit"
+)
+ax.set(title=title_valid_fail_rate)
+ax.set(xlabel=r"$p_\mathrm{err}$", ylabel=title_valid_fail_rate)
+
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("plots/depth_valid_fail_rate_p_err.pdf")
+plt.show()
+
+################## Plot Valid Average Lifetime ##################
+fig, ax = plt.subplots(1, 1, sharex=True)
+
+for i, jid in enumerate(job_ids):
+    code_size = new_dfs[i]["code_size"].iloc[0]
+    stack_depth = new_dfs[i]["stack_depth"].iloc[0]
+    # print(new_dfs[i])
+    ax.scatter(
+        x=new_dfs[i]["p_err"],
+        y=new_dfs[i][key_valid_avg_life],
+        label=f"d={code_size}, h={stack_depth}")
+ax.plot(
+    np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    1.0 / np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    'k',
+    label="One Qubit"
+)
+ax.set(title=title_valid_avg_life)
+ax.set(xlabel=r"$p_\mathrm{err}$", ylabel=title_valid_avg_life, ylim=(0, 10000))
+
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("plots/depth_valid_lifetime_p_err.pdf")
+plt.show()
+
+fig, ax = plt.subplots(1, 1, sharex=True)
+
+for i, jid in enumerate(job_ids):
+    code_size = new_dfs[i]["code_size"].iloc[0]
+    stack_depth = new_dfs[i]["stack_depth"].iloc[0]
+    # print(new_dfs[i])
+    ax.scatter(
+        x=new_dfs[i]["p_err"],
+        y=new_dfs[i][key_valid_avg_life],
+        label=f"d={code_size}, h={stack_depth}")
+ax.plot(
+    np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    1.0 / np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    'k',
+    label="One Qubit"
+)
+ax.set(title=title_valid_avg_life)
+ax.set(
+    xlabel=r"$p_\mathrm{err}$",
+    ylabel=title_valid_avg_life,
+    xlim=(1e-3, new_dfs[0]["p_err"].max()), yscale="log"
+)
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("plots/depth_valid_lifetime_p_err_log.pdf")
+plt.show()
+
+################## Plot Overall Average Lifetime ##################
+fig, ax = plt.subplots(1, 1, sharex=True)
+
+for i, jid in enumerate(job_ids):
+    code_size = new_dfs[i]["code_size"].iloc[0]
+    stack_depth = new_dfs[i]["stack_depth"].iloc[0]
+    # print(new_dfs[i])
+    ax.scatter(
+        x=new_dfs[i]["p_err"],
+        y=new_dfs[i][key_overall_avg_life],
+        label=f"d={code_size}, h={stack_depth}")
+ax.plot(
+    np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    1.0 / np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    'k',
+    label="One Qubit"
+)
+ax.set(title=title_overall_avg_life)
+ax.set(xlabel=r"$p_\mathrm{err}$", ylabel=title_overall_avg_life, ylim=(0, 10000))
+
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("plots/depth_overall_lifetime_p_err.pdf")
+plt.show()
+
+fig, ax = plt.subplots(1, 1, sharex=True)
+
+for i, jid in enumerate(job_ids):
+    code_size = new_dfs[i]["code_size"].iloc[0]
+    stack_depth = new_dfs[i]["stack_depth"].iloc[0]
+    # print(new_dfs[i])
+    ax.scatter(
+        x=new_dfs[i]["p_err"],
+        y=new_dfs[i][key_overall_avg_life],
+        label=f"d={code_size}, h={stack_depth}")
+ax.plot(
+    np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    1.0 / np.linspace(new_dfs[0]["p_err"].min(), new_dfs[0]["p_err"].max(), 100, endpoint=True),
+    'k',
+    label="One Qubit"
+)
+ax.set(title=title_overall_avg_life)
+ax.set(
+    xlabel=r"$p_\mathrm{err}$",
+    ylabel=title_overall_avg_life,
+    xlim=(1e-3, new_dfs[0]["p_err"].max()), yscale="log"
+)
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("plots/depth_overall_lifetime_p_err_log.pdf")
+plt.show()
+
+sys.exit()
+
+
+exit()
+
 
 error_rates = df_all["p_err"]
 key_success_rate = "weighted_success_rate"
