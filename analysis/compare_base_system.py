a9a14264e40fce7a34456918e5c5c5c0751dbff0
mbecker12 <marvinbecker@mail.de>
2021-05-28 14:51:01 +0200

    prepare for d=7 comparative analysis

--- analysis/compare_base_system.py
+++ analysis/compare_base_system.py
@@ -35,14 +35,19 @@ class TrainingRun():
 base_model_config_path="src/config/model_spec/old_conv_agents.json"
 base_model_path="remote_networks/5/65280/simple_conv_5_65280.pt"
 
-training_runs = [
-    TrainingRun(69037, 5, 5, 0.0108, 0.0, "q", "3D Conv", model_name="conv3d"),
-    TrainingRun(71852, 5, 5, 0.008, 0.008, "q", "2D Conv + GRU", model_name="conv2d", model_config_file="conv_agents_slim_gru.json", transfer_learning=1),
-    TrainingRun(69312, 5, 5, 0.01, 0.01, "q", "3D Conv", model_name="conv3d"),
-    TrainingRun(71873, 5, 5, 0.01, 0.01, "ppo", "3D Conv", model_name="conv3d"),
-    TrainingRun(72409, 5, 5, 0.01, 0.01, "q", "2D Conv", model_name="conv2d"),
-]
+# training_runs = [
+#     TrainingRun(69037, 5, 5, 0.0108, 0.0, "q", "3D Conv", model_name="conv3d"),
+#     TrainingRun(71852, 5, 5, 0.008, 0.008, "q", "2D Conv + GRU", model_name="conv2d", model_config_file="conv_agents_slim_gru.json", transfer_learning=1),
+#     TrainingRun(69312, 5, 5, 0.01, 0.01, "q", "3D Conv", model_name="conv3d"),
+#     TrainingRun(71873, 5, 5, 0.01, 0.01, "ppo", "3D Conv", model_name="conv3d"),
+#     TrainingRun(72409, 5, 5, 0.01, 0.01, "q", "2D Conv", model_name="conv2d"),
+# ]
 
+training_runs = [
+    TrainingRun(72411, 7, 7, 0.003, 0.003, "q", "2D Conv", model_name="conv2d"),
+    TrainingRun(69545, 7, 7, 0.005, 0.005, "q", "3D Conv", model_name="conv3d"),
+    TrainingRun(72099, 7, 7, 0.003, 0.003, "q", "2D Conv + GRU", model_name="conv2d", model_config_file="conv_agents_slim_gru.json", transfer_learning=1)
+]
 
 
 plt.rcParams.update({'font.size': 16})
